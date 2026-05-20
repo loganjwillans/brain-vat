e4032d34adb9c71e449ce6dd96eca32cf6a6bf82
loganjwillans <lwillans@DESKTOP-6UITMAL.localdomain>
2026-05-19 21:46:55 -0400

    Quartz sync: May 19, 2026, 9:46 PM

--- quartz.ts
+++ quartz.ts
@@ -3,7 +3,19 @@ import * as ExternalPlugin from "./.quartz/plugins"
 
 ExternalPlugin.Explorer({
   sortFn: (a, b) => {
-    return a.displayName.localeCompare(b.displayName)
+    // 1. If both are files or both are folders, sort them alphabetically
+    if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+      return a.displayName.localeCompare(b.displayName, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
+    }
+    // 2. If 'a' is a file and 'b' is a folder, make the file appear first
+    if (!a.isFolder && b.isFolder) {
+      return -1
+    } else {
+      return 1
+    }
   },
 })
 
