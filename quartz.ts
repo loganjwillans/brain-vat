021c7aa46d571df29c2d9cd12f9adc21f7fe0849
loganjwillans <lwillans@DESKTOP-6UITMAL.localdomain>
2026-05-19 23:06:53 -0400

    Quartz sync: May 19, 2026, 11:06 PM

--- quartz.ts
+++ quartz.ts
@@ -4,16 +4,16 @@ import * as ExternalPlugin from "./.quartz/plugins"
 ExternalPlugin.Explorer({
   sortFn: (a, b) => {
     if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-      return (a.displayName ?? "").localeCompare(b.displayName ?? "", undefined, {
+      return a.displayName.localeCompare(b.displayName, undefined, {
         numeric: true,
         sensitivity: "base",
       })
     }
  
     if (!a.isFolder && b.isFolder) {
-      return 1
-    } else {
       return -1
+    } else {
+      return 1
     }
   },
 })
