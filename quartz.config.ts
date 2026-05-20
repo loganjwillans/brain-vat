085d0ba181903837720ce4823a75e1e04a0d7006
loganjwillans <lwillans@DESKTOP-6UITMAL.localdomain>
2026-05-19 20:09:06 -0400

    Quartz sync: May 19, 2026, 8:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "loganjwillans.github.io",
+    baseUrl: "loganjwillans.github.io/brain-vat",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
